59180b591647993be7faed5d827544d15bfd980f
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-04-12 22:01:35 +0200

    main image added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderMobileComponent } from './header-mobile/header-mobile.component';
 import { MenuMobileComponent } from './menu-mobile/menu-mobile.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderMobileComponent,
-    MenuMobileComponent
+    MenuMobileComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
