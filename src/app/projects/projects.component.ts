dacb32e2789c4acd2c4690aa008ff2ce6bc5ec3d
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-04-18 21:37:12 +0200

    projects array added

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      title: 'Join',
+      stack: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description:
+        'Task manager inspired by the Kanban system. Create and organize tasks by drag and drop, assign other users and set due dates.',
+      image: 'join.png',
+      link_live: '',
+      link_git: '',
+    },
+    {
+      title: 'Sharkie',
+      stack: 'JavaScript | HTML | CSS',
+      description:
+        'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
+      image: 'sharkie.png',
+      link_live: '',
+      link_git: '',
+    },
+    {
+      title: 'Ring of Fire',
+      stack: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description:
+        'Digital version of the popular card game. Play it with your friends on multiple devices at the same time.',
+      image: 'ringoffire.png',
+      link_live: '',
+      link_git: '',
+    },
+  ];
 }
