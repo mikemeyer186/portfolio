0a4719b86b1c184fb6284f17c8d9419696b6a1ad
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-04-23 21:42:06 +0200

    sending mail script added on server

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,6 +7,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  public mailSent: boolean = false;
   public contactForm: FormGroup = new FormGroup({
     sender: new FormControl('', [Validators.required, Validators.minLength(1)]),
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -16,8 +17,21 @@ export class ContactComponent {
     ]),
   });
 
-  sendMail() {
-    console.log(this.contactForm.value);
+  async sendMail() {
+    this.mailSent = true;
+    const sender: string = this.contactForm.value.sender;
+    const email: string = this.contactForm.value.email;
+    const message: string = this.contactForm.value.message;
+    let formData: FormData = new FormData();
+    formData.append('name', sender);
+    formData.append('email', email);
+    formData.append('message', message);
+
+    await fetch('https://mike-meyer.dev/send_mail/send_mail.php', {
+      method: 'POST',
+      body: formData,
+    });
+
     this.contactForm.reset();
   }
 }
