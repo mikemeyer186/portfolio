92a9cef0f09747e516154c81bea493ccf2d8397f
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-04-25 18:48:30 +0200

    aos added on main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
-
+  ngOnInit() {
+    AOS.init();
+  }
 }
