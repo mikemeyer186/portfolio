6e63c8d3fb448191d1306f9ff74f8d771a57243a
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-05-02 22:00:18 +0200

    main redesigned and animation added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import * as AOS from 'aos';
 import { ServiceService } from '../service.service';
 
@@ -8,9 +8,48 @@ import { ServiceService } from '../service.service';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
+  bgHeight: number = 120;
+  bgInterval: any;
+
   constructor(public service: ServiceService) {}
 
   ngOnInit() {
     AOS.init();
+    this.backgroundAnimation();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.backgroundAnimation();
+  }
+
+  isDesktop() {
+    return window.innerWidth > 767;
+  }
+
+  isMobile() {
+    return window.innerWidth < 767;
+  }
+
+  backgroundAnimation() {
+    const backgroundImage: HTMLElement | null =
+      document.getElementById('main-blue-bg');
+    clearInterval(this.bgInterval);
+
+    if (this.isDesktop() && backgroundImage) {
+      this.changesStyleOfImage(backgroundImage);
+    } else if (this.isMobile() && backgroundImage) {
+      backgroundImage.style.height = '120%';
+    }
+  }
+
+  changesStyleOfImage(backgroundImage: HTMLElement) {
+    this.bgHeight = 120;
+    this.bgInterval = setInterval(() => {
+      if (this.bgHeight < 1400) {
+        this.bgHeight++;
+        backgroundImage.style.height = this.bgHeight + 'px';
+      }
+    }, 1000 / 60);
   }
 }
