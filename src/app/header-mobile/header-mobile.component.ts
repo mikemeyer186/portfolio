a4b3780900e5acd4389b1aeaf6670a2b8351ac3c
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-05-02 20:16:56 +0200

    translate for desktop version added

--- src/app/header-mobile/header-mobile.component.ts
+++ src/app/header-mobile/header-mobile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ServiceService } from '../service.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header-mobile',
@@ -7,7 +8,10 @@ import { ServiceService } from '../service.service';
   styleUrls: ['./header-mobile.component.scss'],
 })
 export class HeaderMobileComponent {
-  constructor(public service: ServiceService) {}
+  constructor(
+    public service: ServiceService,
+    public translate: TranslateService
+  ) {}
 
   menuAnimation() {
     if (!this.service.menuIsOpen) {
@@ -15,5 +19,14 @@ export class HeaderMobileComponent {
     } else {
       this.service.closeMenu();
     }
+  }
+
+  changeLanguage(lang: string) {
+    if (lang == 'en') {
+      this.service.langEnglish = true;
+    } else {
+      this.service.langEnglish = false;
+    }
+    this.translate.use(lang);
   }
 }
