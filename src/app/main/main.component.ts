b21cae0e9b7e71196a997c1430023767f0ee11c5
Mike Meyer <mike.meyer86@web.de>
2023-07-26 22:31:08 +0200

    Design improvements and typed.js added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,7 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import * as AOS from 'aos';
 import { ServiceService } from '../service.service';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-main',
@@ -8,48 +9,18 @@ import { ServiceService } from '../service.service';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
-  bgHeight: number = 120;
-  bgInterval: any;
-
   constructor(public service: ServiceService) {}
 
   ngOnInit() {
     AOS.init();
-    this.backgroundAnimation();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.backgroundAnimation();
-  }
-
-  isDesktop() {
-    return window.innerWidth > 767;
-  }
-
-  isMobile() {
-    return window.innerWidth < 767;
-  }
-
-  backgroundAnimation() {
-    const backgroundImage: HTMLElement | null =
-      document.getElementById('main-blue-bg');
-    clearInterval(this.bgInterval);
-
-    if (this.isDesktop() && backgroundImage) {
-      this.changesStyleOfImage(backgroundImage);
-    } else if (this.isMobile() && backgroundImage) {
-      backgroundImage.style.height = '120%';
-    }
-  }
-
-  changesStyleOfImage(backgroundImage: HTMLElement) {
-    this.bgHeight = 120;
-    this.bgInterval = setInterval(() => {
-      if (this.bgHeight < 1800) {
-        this.bgHeight++;
-        backgroundImage.style.height = this.bgHeight + 'px';
-      }
-    }, 1000 / 120);
+    const typed = new Typed('#typed', {
+      strings: ['Frontend Developer', 'Web Developer', 'Software Developer'],
+      typeSpeed: 90,
+      backSpeed: 30,
+      backDelay: 2000,
+      cursorChar: '|',
+      autoInsertCss: true,
+      loop: true,
+    });
   }
 }
